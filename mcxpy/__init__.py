6c4d82befec0eb0e8b1c5036a807ae27e3872e92
Tapanhaz <91151267+Tapanhaz@users.noreply.github.com>
2023-07-12 23:27:58 +0530

    Delete mcxpy directory

--- mcxpy/__init__.py
+++
@@ -1,3 +0,0 @@
-from .mcx import mcx_bhavcopy, mcx_marketwatch, mcx_circulars, mcx_topgainers, mcx_toploosers, mcx_mostactiveoptions, mcx_mostactivecontracts, mcx_heatmap, mcx_optionchain, mcx_pcr, mcx_expiry, mcx_icomdexindices, mcx_quote
-
-__all__ = ['mcx_bhavcopy', 'mcx_marketwatch', 'mcx_circulars', 'mcx_topgainers', 'mcx_toploosers', 'mcx_mostactiveoptions', 'mcx_mostactivecontracts', 'mcx_heatmap', 'mcx_optionchain', 'mcx_pcr', 'mcx_expiry', "mcx_icomdexindices", "mcx_quote"]
