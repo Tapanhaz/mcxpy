c276b415b51597cbcb1b8ef41e896574f8cbef0f
Tapanhaz <91151267+Tapanhaz@users.noreply.github.com>
2023-07-12 23:24:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='mcxpy',
-   version='0.0.1',
+   version='0.0.3',
    description='For fetching mcx data',
    author='Tapan Hazarika',
    packages=['mcxpy'],
